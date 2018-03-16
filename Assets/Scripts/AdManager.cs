c567058eefcc73b97dbbadcfd37aea90aeaf055b
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-03-16 16:02:23 +0100

    Added GoogleAdMob

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -2,26 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GoogleMobileAds;
 using GoogleMobileAds.Api;
 
 
 public class AdManager : MonoBehaviour {
 
-    public static AdManager Instance { set; get; }
-    private string appId = "ca-app-pub-4618159390638701~2163200279";
     private BannerView bannerView;
+    private string appId = "ca-app-pub-4618159390638701~2163200279";
+    public string adUnitId = "ca-app-pub-3940256099942544/6300978111";
 
-	// Use this for initialization
-	private void Start ()
+    // Use this for initialization
+    private void Start ()
     {
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
+        
+        MobileAds.Initialize(appId);
+        this.RequestBanner();
 
-               
-        SceneManager.LoadScene(1);
+       
+
+    
         
 	}
-	
+    void RequestBanner()
+    {
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        AdRequest request = new AdRequest.Builder().Build();
+        bannerView.LoadAd(request);
+
+    }
+    public void ShowBanner()
+    {
+
+    }
+
+    public void ShowVideo()
+    {
+
+    }
 	// Update is called once per frame
 	void Update ()
     {
