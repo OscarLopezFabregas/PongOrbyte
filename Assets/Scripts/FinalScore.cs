f381b3dc9959a04926667812b8f053de722dffdd
OscarLopezFabregas <32035406+OscarLopezFabregas@users.noreply.github.com>
2018-03-21 17:10:17 +0100

    no final score

---
+++ Assets/Scripts/FinalScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour
+{
+    public Text PlayerOneFinalScore;
+    public Text PlayerTwoFinalScore;
+
+    // Use this for initialization
+    void Start()
+    {
+        Scoreboard_Controller.instance.playerOneScoreText = PlayerOneFinalScore;
+        Scoreboard_Controller.instance.playerTwoScoreText = PlayerTwoFinalScore;
+
+    }
+
+
+}
